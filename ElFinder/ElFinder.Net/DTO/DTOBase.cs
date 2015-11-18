346623b5a20e7abe32be5b7af7ab0141b0eb174c
Moshe Fishman <moshe.fishman@gmail.com>
2015-11-18 16:33:56 +0200

    Implemented Folder  & File DNN permissions.

--- ElFinder/ElFinder.Net/DTO/DTOBase.cs
+++ ElFinder/ElFinder.Net/DTO/DTOBase.cs
@@ -65,14 +65,16 @@ namespace ElFinder.DTO
                 throw new ArgumentNullException("info");
             if (root == null)
                 throw new ArgumentNullException("root");
-            //string parentPath = info.Directory.FullName.Substring(root.Directory.FullName.Length);
-            //string relativePath = info.FullName.Substring(root.Directory.FullName.Length);
+
+            var directory = FolderManager.Instance.GetFolder(info.FolderId);
+
+            bool canWrite = DnnFileSystemDriver.HasWritePermissions(directory);
+
             FileDTO response;
             if (root.CanCreateThumbnail(info))
             {
                 ImageDTO imageResponse = new ImageDTO();
                 imageResponse.Thumbnail = root.GetExistingThumbHash(info) ?? (object)1;
-                //var dim = root.GetImageDimension(info);
                 imageResponse.Dimension = string.Format("{0}x{1}", info.Width, info.Height);
                 response = imageResponse;
             }
@@ -81,15 +83,14 @@ namespace ElFinder.DTO
                 response = new FileDTO();
             }
             response.Read = 1;
-            response.Write = root.IsReadOnly ? (byte)0 : (byte)1;
-            response.Locked = (root.LockedFolders.Any(f => f == info.Folder) || root.IsLocked) ? (byte)1 : (byte)0;
+            response.Write = canWrite ? (byte)1 : (byte)0;
+            response.Locked = (!canWrite || root.IsLocked) ? (byte)1 : (byte)0;
             response.Name = info.FileName;
             response.Size = info.Size;
-            response.UnixTimeStamp = info.LastModificationTime.Ticks; //(long)(info.LastWriteTimeUtc - _unixOrigin).TotalSeconds;
+            response.UnixTimeStamp = (long)(info.LastModificationTime - _unixOrigin).TotalSeconds;
             response.Mime = Helper.GetMimeType(info.Extension);
             response.Hash = "F_" + Helper.EncodeId(info.FileId);
             response.ParentHash = "D_" + Helper.EncodeId(info.FolderId);
-            //response.Url = FileManager.Instance.GetUrl(info);
 
             return response;
         }
@@ -102,47 +103,49 @@ namespace ElFinder.DTO
             if (root == null)
                 throw new ArgumentNullException("root");
 
+            bool canWrite = DnnFileSystemDriver.HasWritePermissions(directory);
+
+            bool hasSubdirs = false;
+            var subdirs = FolderManager.Instance.GetFolders(directory);
+            foreach (var item in subdirs)
+            {
+                if (DnnFileSystemDriver.HasReadPermissions(item))
+                {
+                    hasSubdirs = true;
+                    break;
+                }
+            }
+
             if (root.Directory.FolderID == directory.FolderID)
             {
-                bool hasSubdirs = false;
-                var subdirs = FolderManager.Instance.GetFolders(directory);
-                foreach (var item in subdirs)
-                {
-                    if (DnnFileSystemDriver.HasPermissions(item))
-                    {
-                        hasSubdirs = true;
-                        break;
-                    }
-                }
                 RootDTO response = new RootDTO()
                 {
                     Mime = "directory",
                     Dirs = hasSubdirs ? (byte)1 : (byte)0,
                     Hash = "D_" +  Helper.EncodeId(directory.FolderID),
                     Read = 1,
-                    Write = root.IsReadOnly ? (byte)0 : (byte)1,
-                    Locked = root.IsLocked ? (byte)1 : (byte)0,
+                    Write = canWrite ? (byte)1 : (byte)0,
+                    Locked = (!canWrite || root.IsLocked) ? (byte)1 : (byte)0,
                     Name = root.Alias,
                     Size = 0,
-                    UnixTimeStamp = directory.LastUpdated.Ticks// (long)(directory.LastUpdated - _unixOrigin).TotalSeconds,
-//                    VolumeId = root.VolumeId                    
+                    UnixTimeStamp =  (long)(directory.LastUpdated - _unixOrigin).TotalSeconds,
+                    VolumeId = root.VolumeId                    
                 };
                 return response;
             }
             else
             {
-//                string parentPath = directory.Parent.FullName.Substring(root.Directory.FullName.Length);
                 DirectoryDTO response = new DirectoryDTO()
                 {
                     Mime = "directory",
-                    ContainsChildDirs = directory.HasChildren ? (byte)1 : (byte)0,
+                    ContainsChildDirs = hasSubdirs ? (byte)1 : (byte)0,
                     Hash = "D_" + Helper.EncodeId(directory.FolderID),
                     Read = 1,
-                    Write = root.IsReadOnly ? (byte)0 : (byte)1,
-                    Locked = (root.LockedFolders.Any(f => f == directory.FolderName) || root.IsLocked) ? (byte)1 : (byte)0,                    
+                    Write = canWrite ? (byte)1 : (byte)0,
+                    Locked = (!canWrite || root.IsLocked) ? (byte)1 : (byte)0,
                     Size = 0,
                     Name = directory.FolderName,
-                    UnixTimeStamp = directory.LastUpdated.Ticks,// (long)(directory.LastUpdated - _unixOrigin).TotalSeconds,
+                    UnixTimeStamp = (long)(directory.LastUpdated - _unixOrigin).TotalSeconds,
                     ParentHash = "D_" + Helper.EncodeId(directory.ParentID),
                 };
                 return response;
